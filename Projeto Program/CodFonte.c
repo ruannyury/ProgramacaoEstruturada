3532f055c9900d307bafbb5ae2665e90f34d0107
ruannyury <ruannyury1@outlook.com>
2020-09-05 09:22:30 -0300

    Estruturas adicionadas

--- Projeto Program/CodFonte.c
+++ Projeto Program/CodFonte.c
@@ -7,49 +7,79 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <locale.h>
 #include "funcvet.h"
 
 typedef struct {
-    char nome[31];
-    int idade;
-    int nota;
     int codigo;
-}registro_aluno;
+    char nomeString[31];
+}registro_nome;
+
+typedef struct{
+    int codigo;
+    int idade;
+}registro_idade;
+
+typedef struct{
+    int codigo;
+    int notaAlta;
+}registro_nota;
 
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
-    int numElementos;
-    int numAtual;
+    int numElementos; // Número de alunos na sala
+    char nome_sobrenome[41]; // Onde serão armazenados os nomes dos alunos
+    int idade_aluno; 
 
     puts("****PROJETO DE PROGRAMAÇÃO ESTRUTURADA!!!****");
     puts("\nEsse programa irá fazer cálculos estatísticos com os dados obtidos.");
     puts("Dados: EM QUANTAS PROVAS O ALUNO TIROU NOTA ALTA (IGUAL OU SUPERIOR À MÉDIA 6).");
 
-    printf("\nDigite quantos alunos responderam à pesquisa: ");
-    scanf("%d", &numElementos);
+    printf("Quantidade de alunos na classe: "); scanf("%d", &numElementos);
+    printf("\n");
 
-    int vetor[numElementos];
+    int vetor_notas[numElementos]; // Vetor onde as notas dos alunos serão armazenadas
 
-    for (numAtual=0;numAtual<numElementos;numAtual++){
-        printf("Em quantas provas o aluno %d tirou notas igual ou acima da média: ", numAtual + 1);
-        scanf("%d", &vetor[numAtual]);
-    }
+    registro_nome pessoa[numElementos]; /* Declara uma variável vetor estrutura do tipo 
+                                           registro_nome com o número de elementos igual
+                                           ao número de alunos. */
 
-    ordemCrescente(vetor,numElementos);
-    impressao(vetor,numElementos);
+    registro_idade idade[numElementos]; /* Declara uma variável vetor estrutura do tipo 
+                                           registro_idade com o número de elementos igual
+                                           ao número de alunos. */
+   
+    int j = 10;
 
-    puts("******EIS AS ESTATÍSTICAS DOS DADOS:******");
+    for(int i = 0; i < numElementos; ++i){ // Roda o loop tanto quanto for o número de alunos na sala
+        /*
+            Armazenando cada nome da variável nome_sobrenome na variável 
+            pessoa[i].nomeString com a função strcpy():
+        */
+        printf("Nome e sobrenome do aluno %d:\n", i + 1); scanf(" %[^\n]", nome_sobrenome);
 
-    printf("\nO maior valor é: %d", maiorvalor(vetor,numElementos));
-    printf("\nO menor valor é: %d", menorvalor(vetor));
-    printf("\nA amplitude é: %d", amplitude(vetor[0],maiorvalor(vetor,numElementos)));
-    printf("\nA média é: %f", media(vetor,numElementos));
-    printf("\nA mediana é: %.2f", mediana(vetor,numElementos));
-    printf("\nO desvio padrão é: %f\n", desvioPadrao(vetor,numElementos,media(vetor,numElementos)));
+        pessoa[i].codigo = j; strcpy(pessoa[i].nomeString, nome_sobrenome);
 
-    frequencia(vetor, numElementos);
+        /*
+            Armazenando cada idade da variável idade_aluno na variável 
+            idade[i].codigo:
+        */
+        printf("Idade: "); scanf("%d", &idade_aluno);
+        idade[i].codigo = i + 1; idade[i].idade = idade_aluno; 
+
+        /*
+            Armazenando as notas desses alunos em vetor_notas:
+        */
+        printf("Número de notas iguais ou superiores à média 6: "); scanf(" %d", &vetor_notas[i]);
+        
+        j += 10; // Encrementa a variável responsável pelo código de cada aluno
+        
+    }    
+
+
+
+    return 0;
 } 
